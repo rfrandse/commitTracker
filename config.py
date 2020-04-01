6eab6a7dae4714bd8aed7b740e96a8e081a8e3fa
Reed Frandsen <rfrandse@us.ibm.com>
2020-04-01 17:01:17 -0500

    add commitTracker.py and template of config.py files

---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+#config.py only update local copy don't save to public
+py_token = "put token here"
+py_ibm_token = "put token here"
+
+auth_headers = {'Authorization': 'token ' +  py_token }
+
+
+
